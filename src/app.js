3b9918d79cf5f6c50717d5035f65e578615ca743
Piyush Singh <piyushsingh8796@gmail.com>
2018-09-30 11:49:30 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,18 +2,13 @@ const  fs=require('fs');
 const path=require('path');
 const express=require('express');
 const app=express();
+const {accounts, users, writeJSON }=require('./data') ;
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({ extended: true }));
 
-const accountData=fs.readFileSync(path.join(__dirname,'json','accounts.json'),'utf-8');
-const accounts=JSON.parse(accountData);
-
-
-const userData=fs.readFileSync(path.join(__dirname,'json','users.json'),'utf-8');
-const users=JSON.parse(userData);
 
 app.get('/',(req,res) =>{
   res.render('index',{ title: 'Account Summary', accounts});
@@ -28,7 +23,7 @@ app.get('/checking',(req,res) =>{ res.render('account' , { account: accounts.che
 app.get('/credit',(req,res) =>{ res.render('account' , { account: accounts.credit});
 });
 
-app.get('/profile',(req,res) => { res.render('profile',{ user: users[0]});
+app.get('/profile',(req,res) => { res.render( 'profile' ,{ user: users[0]});
 });
 
 
@@ -38,8 +33,7 @@ app.post('/transfer', (req,res) => {
 
   accounts[req.body.from].balance-=req.body.amount;
   accounts[req.body.to].balance= parseInt(accounts[req.body.to].balance)+parseInt(req.body.amount,10);
-    const accountsJSON= JSON.stringify(accounts,null,4);
-    fs.writeFileSync(path.join(__dirname,'json/accounts.json'), accountsJSON, 'utf-8');
+     writeJSON();
     res.render('transfer' ,{ message: 'Transfer Completed'});
 });
 
@@ -51,9 +45,8 @@ app.post('/payment', (req,res) => {
 
   accounts.credit.balance-=req.body.amount;
   accounts.credit.available= parseInt(accounts.credit.available)+parseInt(req.body.amount,10);
-    const accountsJSON= JSON.stringify(accounts,null,4);
-    fs.writeFileSync(path.join(__dirname,'json/accounts.json'), accountsJSON, 'utf-8');
-    res.render('payment' ,{ message: 'Payment Successfull ',   account: accounts.credit });
+     writeJSON();
+     res.render('payment' ,{ message: 'Payment Successfull ',   account: accounts.credit });
 });
 
 
