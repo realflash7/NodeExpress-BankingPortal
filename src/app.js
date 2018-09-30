8045942f22caa312e94839d73893f6aa2f917e7a
Piyush Singh <piyushsingh8796@gmail.com>
2018-09-30 09:45:59 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,34 +5,55 @@ const app=express();
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({ extended: true }));
 
 const accountData=fs.readFileSync(path.join(__dirname,'json','accounts.json'),'utf-8');
 const accounts=JSON.parse(accountData);
+
+
 const userData=fs.readFileSync(path.join(__dirname,'json','users.json'),'utf-8');
 const users=JSON.parse(userData);
-
-
-app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res) =>{
   res.render('index',{ title: 'Account Summary', accounts});
 });
 
-app.get('/savings',(req,res) =>{
-  res.render('account', { account: accounts.savings});
+app.get('/savings',(req,res) =>{ res.render('account', { account: accounts.savings});
 });
 
-app.get('/checking',(req,res) =>{
-    res.render('account' , { account: accounts.checking});
+app.get('/checking',(req,res) =>{ res.render('account' , { account: accounts.checking});
+});
+
+app.get('/credit',(req,res) =>{ res.render('account' , { account: accounts.credit});
+});
+
+app.get('/profile',(req,res) => { res.render('profile',{ user: users[0]});
 });
 
 
-app.get('/credit',(req,res) =>{
-    res.render('account' , { account: accounts.credit});
+app.get('/transfer',(req,res) => { res.render('transfer')});
+
+app.post('/transfer', (req,res) => {
+
+  accounts[req.body.from].balance-=req.body.amount;
+  accounts[req.body.to].balance= parseInt(accounts[req.body.to].balance)+parseInt(req.body.amount,10);
+    const accountsJSON= JSON.stringify(accounts,null,4);
+    fs.writeFileSync(path.join(__dirname,'json/accounts.json'), accountsJSON, 'utf-8');
+    res.render('transfer' ,{ message: 'Transfer Completed'});
 });
 
-app.get('/profile',(req,res) => {
-  res.render('profile',{ user: users[0]});
+
+app.get('/payment',(req,res) => { res.render('payment')});
+
+app.post('/payment', (req,res) => {
+
+  accounts.credit.balance-=req.body.amount;
+  accounts.credit.available= parseInt(accounts.credit.available)+parseInt(req.body.amount,10);
+    const accountsJSON= JSON.stringify(accounts,null,4);
+    fs.writeFileSync(path.join(__dirname,'json/accounts.json'), accountsJSON, 'utf-8');
+    res.render('payment' ,{ message: 'Payment Successfull;', account: accounts.credit });
 });
+
 
 app.listen(3000,() => console.log('Shivam Project Running on local host'));
