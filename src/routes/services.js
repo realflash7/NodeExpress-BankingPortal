c7c7fb17424c862ca37d428aa844388edc87b39e
Piyush Singh <piyushsingh8796@gmail.com>
2018-09-30 12:09:55 +0530

    Create services.js

---
+++ src/routes/services.js
@@ -0,0 +1,27 @@
+const expres=require('express');
+const router=express.Router();
+const {accounts,writeJSON }=reqire('../data');
+
+router.get('/transfer',(req,res) => { res.render('transfer')});
+
+router.post('/transfer', (req,res) => {
+
+  accounts[req.body.from].balance-=req.body.amount;
+  accounts[req.body.to].balance= parseInt(accounts[req.body.to].balance)+parseInt(req.body.amount,10);
+     writeJSON();
+    res.render('transfer' ,{ message: 'Transfer Completed'});
+});
+
+
+router.get('/payment',(req,res) => { res.render('payment', {account: accounts.credit});
+ });
+
+router.post('/payment', (req,res) => {
+
+  accounts.credit.balance-=req.body.amount;
+  accounts.credit.available= parseInt(accounts.credit.available)+parseInt(req.body.amount,10);
+     writeJSON();
+     res.render('payment' ,{ message: 'Payment Successfull ',   account: accounts.credit });
+});
+
+module.exports=router;
