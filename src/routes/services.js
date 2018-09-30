1146f364409bf2d09cc8a24fd74f53f467b20c8e
Piyush Singh <piyushsingh8796@gmail.com>
2018-09-30 12:25:50 +0530

    Update services.js

--- src/routes/services.js
+++ src/routes/services.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const router=express.Router();
-const {accounts,writeJSON }=reqire('../data');
+const {accounts,writeJSON }=require('../data');
 
 router.get('/transfer',(req,res) => { res.render('transfer')});
 
