0d271ebf66f493071778c26539893b1db8b102e3
Piyush Singh <piyushsingh8796@gmail.com>
2018-09-30 12:27:23 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express=require('express');
 const app=express();
 const {accounts, users, writeJSON }=require('./data') ;
 
-const accountRoutes=require('./routes/account');
+const accountRoutes=require('./routes/accounts');
 const servicesRoutes=require('./routes/services');
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
