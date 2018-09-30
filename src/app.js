161c319ab0554c8992319a9251608df1f391c734
Piyush Singh <piyushsingh8796@gmail.com>
2018-09-30 12:11:21 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const express=require('express');
 const app=express();
 const {accounts, users, writeJSON }=require('./data') ;
 
+const accountRoutes=require('./routes/account');
+const servicesRoutes=require('./routes/services');
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname,'public')));
@@ -14,40 +16,13 @@ app.get('/',(req,res) =>{
   res.render('index',{ title: 'Account Summary', accounts});
 });
 
-app.get('/savings',(req,res) =>{ res.render('account', { account: accounts.savings});
-});
+app.use('/account' ,accountRoutes);
+app.use('/services', servicesRoutes);
 
-app.get('/checking',(req,res) =>{ res.render('account' , { account: accounts.checking});
-});
-
-app.get('/credit',(req,res) =>{ res.render('account' , { account: accounts.credit});
-});
 
 app.get('/profile',(req,res) => { res.render( 'profile' ,{ user: users[0]});
 });
 
-
-app.get('/transfer',(req,res) => { res.render('transfer')});
-
-app.post('/transfer', (req,res) => {
-
-  accounts[req.body.from].balance-=req.body.amount;
-  accounts[req.body.to].balance= parseInt(accounts[req.body.to].balance)+parseInt(req.body.amount,10);
-     writeJSON();
-    res.render('transfer' ,{ message: 'Transfer Completed'});
-});
-
-
-app.get('/payment',(req,res) => { res.render('payment', {account: accounts.credit});
- });
-
-app.post('/payment', (req,res) => {
-
-  accounts.credit.balance-=req.body.amount;
-  accounts.credit.available= parseInt(accounts.credit.available)+parseInt(req.body.amount,10);
-     writeJSON();
-     res.render('payment' ,{ message: 'Payment Successfull ',   account: accounts.credit });
-});
 
 
 app.listen(3000,() => console.log('Shivam Project Running on local host'));
